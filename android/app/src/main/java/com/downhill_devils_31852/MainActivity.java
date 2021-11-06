460e4d0d316a7a9f68512f66ae25cc00562f0ace
Crowdbotics <team@crowdbotics.com>
2021-11-06 20:42:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/downhill_devils_31852/MainActivity.java
@@ -0,0 +1,15 @@
+package com.downhill_devils_31852;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "downhill_devils_31852";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "downhill_devils_31852",
+  url: "https://downhill_devils_31852.botics.co",
+  api: "https://downhill_devils_31852.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
